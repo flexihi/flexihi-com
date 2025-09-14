d84a8940657526e8ab1e0417b0c612af16e47ad2
Salmi <nassalmi.com@gmail.com>
2025-09-14 07:30:32 +0400

    /ghasilhub redirect

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/ghasilhub',
+        destination: 'https://website-omega-seven-65.vercel.app/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
